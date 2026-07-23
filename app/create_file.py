7c16a6b81e08761dcb4b0520e0aa4f306b3a34d9
Nadiia Miziukanova <mizukanovanadya@gmail.com>
2026-07-23 22:41:36 +0300

    Solution finished

--- app/create_file.py
+++ app/create_file.py
@@ -3,44 +3,6 @@ import sys
 import datetime
 
 
-def create_file() -> None:
-    if len(sys.argv) < 2:
-        return
-
-    flag = sys.argv[1]
-    both_flags = sys.argv
-    if "-f" in both_flags and "-d" in both_flags:
-        f_index = sys.argv.index("-f")
-        d_index = sys.argv.index("-d")
-
-        if f_index > d_index:
-            dir_parts = sys.argv[2:f_index]
-            file_name = sys.argv[f_index + 1]
-
-        elif f_index < d_index:
-            file_name = sys.argv[f_index + 1]
-            dir_parts = sys.argv[d_index + 1:]
-
-        current_path = ""
-        for part in dir_parts:
-            current_path = os.path.join(current_path, part)
-
-        os.makedirs(current_path, exist_ok=True)
-        file_path = os.path.join(current_path, file_name)
-        write_content(file_path)
-
-    elif flag == "-d":
-        current_path = ""
-
-        for part in sys.argv[2:]:
-            current_path = os.path.join(current_path, part)
-        os.makedirs(current_path, exist_ok=True)
-
-    elif flag == "-f":
-        file_name = sys.argv[2]
-        write_content(file_name)
-
-
 def write_content(file_path: str) -> None:
     with open(file_path, "a") as new_file:
         counter = 1
@@ -49,7 +11,7 @@ def write_content(file_path: str) -> None:
 
         if os.path.getsize(file_path) != 0:
             new_file.write("\n")
-            new_file.write(str(timestamp) + "\n")
+        new_file.write(str(timestamp) + "\n")
 
         while True:
             text = input("Enter content line: ")
@@ -59,5 +21,37 @@ def write_content(file_path: str) -> None:
             counter += 1
 
 
-if __name__ == "__main__":
-    create_file()
+if len(sys.argv) < 3:
+    sys.exit(1)
+flag = sys.argv[1]
+both_flags = sys.argv
+if "-f" in both_flags and "-d" in both_flags:
+    f_index = sys.argv.index("-f")
+    d_index = sys.argv.index("-d")
+
+    if f_index > d_index:
+        dir_parts = sys.argv[2:f_index]
+        file_name = sys.argv[f_index + 1]
+
+    elif f_index < d_index:
+        file_name = sys.argv[f_index + 1]
+        dir_parts = sys.argv[d_index + 1:]
+
+    current_path = ""
+    for part in dir_parts:
+        current_path = os.path.join(current_path, part)
+
+    os.makedirs(current_path, exist_ok=True)
+    file_path = os.path.join(current_path, file_name)
+    write_content(file_path)
+
+elif flag == "-d":
+    current_path = ""
+
+    for part in sys.argv[2:]:
+        current_path = os.path.join(current_path, part)
+    os.makedirs(current_path, exist_ok=True)
+
+elif flag == "-f":
+    file_name = sys.argv[2]
+    write_content(file_name)
